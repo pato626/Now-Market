c4584a8b34736ef87ded2a7ff1318a911fc20db9
pato <ruromar666@gmail.com>
2024-08-12 12:30:37 -0300

    not last commit3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ["react-router-dom"]
+      external: 
+      ["react-router-dom"]
+      ['react-hook-form']
     }
   }
 });
