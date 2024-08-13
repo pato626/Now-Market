a41d6217551202809552f8fa0a464355b15eb3b7
pato <ruromar666@gmail.com>
2024-08-12 21:59:21 -0300

    test 5

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     rollupOptions: {
-      external: []
-    }
-  }
-})
+      external: ['react-hook-form'],
+    },
+  },
+  optimizeDeps: {
+    include: ['react-hook-form'],
+  },
+});
